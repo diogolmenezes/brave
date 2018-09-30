96bed45d4e344466aea3312f3d12803a84631a2f
Diogo Menezes <diogolmenezes@gmail.com>
2018-09-30 18:49:38 -0300

    ajustes

--- client/src/components/pages/homePage.jsx
+++ client/src/components/pages/homePage.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import { withRouter} from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 class HomePage extends Component {
     constructor(props) {
-        super(props);        
+        super(props);
 
         this.state = {
             session: null,
@@ -20,14 +20,24 @@ class HomePage extends Component {
     }
 
     render() {
-
-
         return (
             <div className="container center-align">
-                <img className="circle responsive-img" src={ require('../../images/brave2.jpg') } />
-                <h2>Desde 2016 fazendo as perguntas que precisam ser feitas.</h2>
-                <p>Tem certeza que você não está esquecendo de nada? Ahh duvido!</p>
-                <button className="waves-effect waves-light btn-large" onClick={this.start.bind(this)}>Começar</button>
+                <div className="row">
+                    <div className="col s12">
+                        <img className="circle responsive-img" src={require('../../images/brave2.jpg')} />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s12">
+                        <h2>Desde 2016 fazendo as perguntas que precisam ser feitas.</h2>
+                        <p>Tem certeza que você não está esquecendo de nada? Ahh duvido!</p>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col s12">
+                        <button className="waves-effect waves-light btn-large" onClick={this.start.bind(this)}>Começar</button>
+                    </div>
+                </div>
             </div>
         );
     }
